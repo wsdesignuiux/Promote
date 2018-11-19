581d576f28a61ddb7d56dd3c39d80e25a02f4ebe
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-19 17:00:39 +0530

    Initial commit

---
+++ app/src/main/java/fragement/Automotive.java
@@ -0,0 +1,25 @@
+package fragement;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vaibhav.promote.R;
+
+
+/**
+ * Created by wolfsoft4 on 21/8/18.
+ */
+
+public class Automotive extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.automotive,container,false);
+        return view;
+    }
+}
